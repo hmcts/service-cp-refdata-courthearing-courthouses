03b58f3613cfd910940468876a36ed8a12b09994
Dmitri Algazin <dmitri.algazin@hmcts.net>
2025-04-10 13:31:35 +0100

    project setup

--- src/main/java/uk/gov/hmcts/cp/controllers/CourtHousesController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/CourtHousesController.java
@@ -4,18 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.cp.openapi.api.DefaultApi;
+import uk.gov.hmcts.cp.openapi.api.CourtHouseApi;
 import uk.gov.hmcts.cp.openapi.model.CourtHousesschema;
 import uk.gov.hmcts.cp.services.CourtHousesService;
 
 @RestController
 @RequiredArgsConstructor
-public class CourtHousesController implements DefaultApi {
+public class CourtHousesController implements CourtHouseApi {
 
     private final CourtHousesService courtHousesService;
 
     @Override
-    public ResponseEntity<CourtHousesschema> courthousesCourtIdGet(String courtId) {
+    public ResponseEntity<CourtHousesschema> getCourthouseByCourtId(String courtId) {
         CourtHousesschema courtHousesschema = courtHousesService.getCourtHouse(courtId);
         return new ResponseEntity<>(courtHousesschema, HttpStatus.OK);
     }
