ed31b8f4c2d00cb6fb628f0b1c6dc385ebeb0cac
Dmitri Algazin <Dmitri.Algazin@hmcts.net>
2025-05-30 15:03:47 +0100

    feat: moving to main branch;

--- src/main/java/uk/gov/hmcts/cp/services/CourtHousesService.java
+++ src/main/java/uk/gov/hmcts/cp/services/CourtHousesService.java
@@ -1,18 +1,25 @@
 package uk.gov.hmcts.cp.services;
 
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.cp.openapi.model.CourtHousesschema;
-import uk.gov.hmcts.cp.openapi.model.CourtHousesschemaCourtHouse;
+import uk.gov.hmcts.cp.openapi.model.CourtHouse;
+import uk.gov.hmcts.cp.openapi.model.CourtHouseCourtRoomInner;
+
+import java.util.List;
 
 @Service
 public class CourtHousesService {
 
-    public CourtHousesschema getCourtHouse(String courtId) {
-        CourtHousesschema courtHousesschema = new CourtHousesschema();
-        CourtHousesschemaCourtHouse courtHouse = new CourtHousesschemaCourtHouse();
-        courtHouse.courtHouseName("House name 221B");
+    public CourtHouse getCourtHouse(String courtId) {
+        CourtHouse courtHouse = new CourtHouse();
         courtHouse.courtHouseCode(courtId);
-        courtHousesschema.courtHouse(courtHouse);
-        return courtHousesschema;
+        courtHouse.courtHouseName("House name 221B");
+        courtHouse.courtHouseType(CourtHouse.CourtHouseTypeEnum.CROWN);
+        courtHouse.courtHouseDescription("House name 221B description");
+        CourtHouseCourtRoomInner houseCourtRoom = new CourtHouseCourtRoomInner();
+        houseCourtRoom.courtRoomId(1);
+        houseCourtRoom.courtRoomNumber(1);
+        houseCourtRoom.courtRoomName("Room 1");
+        courtHouse.courtRoom(List.of(houseCourtRoom));
+        return courtHouse;
     }
 }
