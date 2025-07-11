18ef31b535046d18842f96038cf645c3a3742b48
Dmitri Algazin <dmitri.algazin@hmcts.net>
2025-07-11 15:37:15 +0100

    feat: fixing Dockerfile; (#58)

--- src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/helper/JsonFileToObject.java
+++ src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/helper/JsonFileToObject.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.cp.pact.helper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +11,12 @@ import java.util.Objects;
 public class JsonFileToObject {
 
     private static final Logger LOG = LoggerFactory.getLogger(JsonFileToObject.class);
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper()
+        .registerModule(new JavaTimeModule());
 
     public static <T> T readJsonFromResources(String fileName, Class<T> clazz) throws Exception {
         File file;
-        try{
+        try {
             file = new File(Objects.requireNonNull(JsonFileToObject.class.getClassLoader().getResource(fileName)).toURI());
         } catch (Exception e) {
             LOG.atError().log("Error loading file: {}", fileName, e);
