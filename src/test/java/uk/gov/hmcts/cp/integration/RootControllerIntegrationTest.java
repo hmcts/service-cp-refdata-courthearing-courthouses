56b96ade276866d32a9445ec9b529424de032a3a
madhusudan.yasa <madhusudan.yasa@hmcts.net>
2025-12-17 11:05:30 +0000

    AMP-122 - deleted integration module.

--- src/test/java/uk/gov/hmcts/cp/integration/RootControllerIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/integration/RootControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.cp.controllers;
+package uk.gov.hmcts.cp.integration;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,39 +6,52 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import uk.gov.hmcts.cp.config.IntegrationTestConfig;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Import(IntegrationTestConfig.class)
 @TestPropertySource(properties = {
     "service.court-house-client.url=https://CAOURT_HOUSE.org.uk",
     "service.court-house-client.cjscppuid=MOCK-CJSCPPUID"
 })
-class CourtHousesControllerTest {
+class RootControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("Should /courthouses/{court_id}/courtrooms/{court_room_id} request with 200 response code")
+    @DisplayName("Should welcome upon root request with 200 response code")
     @Test
-    void shouldCallActuatorAndGet200() throws Exception {
-        final MvcResult result = mockMvc.perform(get("/courthouses/123/courtrooms/123"))
+    void shouldCallRootAndGet200() throws Exception {
+
+        final MvcResult result = mockMvc.perform(get("/"))
             .andDo(print())
             .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Welcome to service-cp-refdata-courthearing-courthouses")))
             .andReturn();
+        assertThat(result).isNotNull();
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
 
+    @DisplayName("Actuator health status should be UP")
+    @Test
+    void shouldCallActuatorAndGet200() throws Exception {
+        final MvcResult result = mockMvc.perform(get("/health"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("UP"))
+            .andReturn();
         assertThat(result).isNotNull();
         assertThat(result.getResponse().getStatus()).isEqualTo(200);
     }
